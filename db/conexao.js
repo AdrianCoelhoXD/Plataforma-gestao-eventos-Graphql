d4cd9d69a342c5149ce431bd64b7fd1ae1f0556c
Adrian Coelho <adriantwocoelho2010@outlook.com>
2025-03-12 15:25:59 -0300

    Ajustes

--- db/conexao.js
+++ db/conexao.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//MONGODB_URI=mongodb://localhost:27017/gerenciador-eventos 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
